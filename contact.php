750756e33d6150cc846dbcc2f68f57907a19d179
guirald <guiralmaloneespaldon.escober@bicol-u.edu.ph>
2023-05-20 17:04:19 +0800

    more info in portfolio and started contact

--- contact.php
+++ contact.php
@@ -12,7 +12,24 @@
     <body>
 
         <div id="header"></div>
-        
+
+        <div class="titlecontacts">  
+            <p>Contact Me</p>
+        </div>
+
+        <div class="row justify-content-center text-center">
+            <div class="group col-3" style="border: solid; border-color:white;">
+            yes
+            </div>
+            
+            <div class="group col-3" style="border: solid; border-color:white;">
+            yes  
+            </div>
+            
+            <div class="group col-3" style="border: solid; border-color:white;">
+            yes
+            </div>
+        </div>
         
     </body>
 
--- portfolio.php
+++ portfolio.php
@@ -4,21 +4,53 @@
     <script> 
             $(document).ready(function(){
                     $(".WelcomeContainer").fadeIn(6000);
-                });
-        
-            
+                });            
         </script>
     </head>
 
     <body>
         <div id="header"></div>
-        <div class="WelcomeContainer">
-            
-            <div id="WelcomeLand">
-                <p>Welcome To My Portfolio</p>
-            </div>
+        
+        <div class="container-main">  
+            <p>Welcome To My Portfolio</p>
         </div>
-     
+
+        <div class="card-grouper">
+
+            <h2 class="title">Cover Pages Done During 12th Grade</h2>
+                
+            <div class="card-group">
+
+                <div class="card col">
+                    <img src="/public/biol.png" alt="">
+                </div>
+
+                <div class="card col">
+                    <img src="/public/biol.png" alt="">
+                </div>
+                    
+                <div class="card col">
+                    <img src="/public/biol.png" alt="">
+                </div>
+
+            </div>
+
+            <h2 class="title">Science And Technology Fair Paper</h2>
+            <div class="card-group" style="justify-content:center">
+                <div>
+                    <a><img src="/public/sample.png" alt="" style="margin-top:8vh; height:100vh; width:35vw;"></a>
+
+                </div>
+            </div>
+
+            <h2 class="title">OOP Inventory Management Project</h2>
+            <div class="card-group" style="justify-content:center">
+                <div style="flex: auto;">
+                    <a href="https://github.com/Gappsy/OOPprojBread"><img src="/public/bread.png" alt="" style="margin-top:8vh; height:100vh; width:70vw;"></a>
+
+                </div>
+            </div>
+        </div><!-- End Div of main container-->
     </body>
 
 </html>
